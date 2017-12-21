8c74c3895052e5830667c7397d0a9e22b707e249
adrianus johan <adrianus@go-jek.com>
2017-12-21 11:28:35 +0700

    revisi

--- send-email.php
+++ send-email.php
@@ -24,8 +24,8 @@ $mail = new PHPMailer;
 $mail->isSMTP();                                // Set mailer to use SMTP
 $mail->Host = ' smtp.gmail.com';  				// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                        	// Enable SMTP authentication
-$mail->Username = "adlybast@gmail.com";                		// SMTP username
-$mail->Password = "boxiast1234";                   		// SMTP password
+$mail->Username = "your email";                		// SMTP username
+$mail->Password = "your pass";                   		// SMTP password
 $mail->SMTPSecure = 'tls';                     	// Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                             	// TCP port to connect to
 
